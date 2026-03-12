a67ebfeff254142f960b045c6aafe5e88358f0d1
Coko <91132775+Coko7@users.noreply.github.com>
2026-03-12 18:01:14 +0100

    feat: add fwin script

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# f-win
-A handy script to "fix" Windows by installing decent CLIs and apps.
+# ✨ f-win
+
+A handy script to "*fix*" Windows by installing decent CLIs and apps.
+
+## 🛠️ Setup / Usage
+
+Run it with one command:
+```ps1
+irm https://get.coko7.fr/fwin.ps1 | iex
+```
+
+## 🐛 Issues
+
+### Cannot resolve 7-zip hostname
+
+Scoop needs [7-zip](https://www.7-zip.org/) to install packages correctly.
+Some organizations/countries tend to block 7-zip for whatever reasons. If you find yourself in this spot, you can try
+to install 7-zip manually via copying MSI and then instructing scoop to use an external binary:
+```ps1
+scoop config use_external_7zip true
+```
+For more info: https://github.com/ScoopInstaller/Scoop/discussions/6553
+
+### Problem with schannel
+
+If you run into the following:
+```txt
+Updating Scoop...
+fatal: unable to access 'https://github.com/ScoopInstaller/Scoop/': schannel: next InitializeSecurityContext failed: CRYPT_E_REVOCATION_OFFLINE (0x80092013) - The revocation function was unable to check revocation because the revocation server was offline.
+```
+You may want to switch the git SSL backend to OpenSSL:
+```ps1
+git config --global http.sslBackend openssl
+```
+Learn more here: https://stackoverflow.com/questions/45556189/git-the-revocation-function-was-unable-to-check-revocation-for-the-certificate
---
+++ fwin.ps1
@@ -0,0 +1,89 @@
+#  _____           _     __        ___           _
+# |  ___|   _  ___| | __ \ \      / (_)_ __   __| | _____      _____
+# | |_ | | | |/ __| |/ /  \ \ /\ / /| | '_ \ / _` |/ _ \ \ /\ / / __|
+# |  _|| |_| | (__|   <    \ V  V / | | | | | (_| | (_) \ V  V /\__ \
+# |_|   \__,_|\___|_|\_\    \_/\_/  |_|_| |_|\__,_|\___/ \_/\_/ |___/
+#
+# "Windows sucks ass, lets fix it for good." ~ @me
+#
+# Version 0.1.0
+# Author: @coko7
+
+# First, we install Scoop because for some fucking reason, some versions of Windows server do not come with winget by default.
+# ...
+# Ha. Ha.
+if (-not (Get-Command scoop -ErrorAction SilentlyContinue))
+{
+    Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
+    Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
+}
+
+# Then, we install decent scoop buckets
+$buckets = @("main", "extras")
+foreach ($bucket in $buckets)
+{
+    if (-not (scoop bucket list | Select-String $bucket))
+    {
+        scoop bucket add $bucket
+        Write-Output "Added scoop bucket: $bucket"
+    }
+}
+
+function Ensure-Installed
+{
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]$AppName
+    )
+
+    if (-not (scoop list $AppName 2>$null | Select-String -Quiet "^$AppName(\s|$)"))
+    {
+        Write-Host "Installing $AppName..."
+        scoop install $AppName
+    } else
+    {
+        Write-Host "$AppName is already installed, skipping."
+    }
+}
+
+# Install git
+Ensure-Installed main/git
+
+# Then, we install fucking terminal editors cause there are none installed by default?...
+# HELLO??? WINDOWS """SERVER""". What does the word "server" in "Windows server" stand for exactly?
+# wtf Microsoft, get your shit together already.
+Ensure-Installed main/neovim
+Ensure-Installed main/edit
+
+# Now we install atuin to easily fuzzy search shell history
+if (!(scoop which atuin))
+{
+    Ensure-Installed main/atuin
+    if (-not (Test-Path -Path $PROFILE))
+    {
+        New-Item -ItemType File -Path $PROFILE -Force | Out-Null
+    }
+    Write-Output 'atuin init powershell | Out-String | Invoke-Expression' >> $PROFILE
+}
+
+# Now we setup zoxide to magically traverse your fs
+if (!(scoop which zoxide))
+{
+    Ensure-Installed main/zoxide
+    Write-Output 'Invoke-Expression (& { (zoxide init powershell | Out-String) })' >> $PROFILE
+}
+
+# Here are some more fire CLIs:
+Ensure-Installed main/ripgrep # blazingly fast grepping
+Ensure-Installed main/fzf # the greatest command-line fuzzy finder
+Ensure-Installed main/bat # like UNIX cat but fancier
+Ensure-Installed main/delta # a syntax-highlighting pager for git, diff, grep, and blame output
+Ensure-Installed main/fd # modern alternative to UNIX find command
+Ensure-Installed main/xh # modern alternative to cURL
+
+# Finally, lets install some decent GUI apps
+Ensure-Installed extras/windows-terminal
+
+Write-Host '🚀 Congratulations. Your Windows dev setup is now a tiny bit better ✨'
+Write-Host "🐧 Don't forget to switch over to Linux when you get a chance 👋"
+
